5d96ff31ab82436428579e8e837df78657b2a751
Karina Titov <karinasul1995@gmail.com>
2020-05-16 09:43:33 -0500

    heroku test

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './public/assests/src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/bundle.js'
